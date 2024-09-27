e3c859bbdfe11229993900f1686a19fa87e9664f
abrar997 <abraralrawi997.com>
2024-09-27 12:43:59 +0300

    working at three steps

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#1F1F1F",
+        content: "#545454",
+      },
+      fontSize: {
+        "6xl": "100px",
+        paragraph: "22px",
+      },
+      fontFamily: {
+        main: "Inter",
+      },
+      lineHeight: {
+        xl: "70.19px",
+      },
+    },
+  },
+  plugins: [],
+};
